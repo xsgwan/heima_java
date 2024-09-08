eacb4c71f3e84346350d7c852438df9bff2e7fbf
ghh <ghhayl520@gmail.com>
2024-09-09 06:25:49 +0800

    static: 代码块

---
+++ project/javasepromax/day01-oop-app/src/com/gyy/block/Student.java
@@ -0,0 +1,31 @@
+package com.gyy.block;
+
+public class Student {
+    static int number = 80;
+    static String schoolName;
+
+    // 静态代码块
+    static {
+        System.out.println("静态代码块执行了");
+        schoolName = "gyy";
+    }
+
+    int age;
+
+    // 实例代码块
+    {
+        System.out.println("实例代码块执行了");
+        // age = 18;
+        System.out.println("有人创建了对象:" + this);
+    }
+
+    public Student() {
+        System.out.println("无参数构造器执行了");
+    }
+
+    public Student(String name) {
+        System.out.println("有参数构造器执行了");
+    }
+
+
+}
---
+++ project/javasepromax/day01-oop-app/src/com/gyy/block/Test.java
@@ -0,0 +1,20 @@
+package com.gyy.block;
+
+import java.net.Socket;
+
+public class Test {
+    public static void main(String[] args) {
+        // 目标:认识两种代码块，了解他们的特点和基本作用
+        System.out.println(Student.number);
+        System.out.println(Student.number);
+        System.out.println(Student.number);
+
+        System.out.println(Student.schoolName); // gyy
+
+        System.out.println("--------------------------");
+        Student s1 = new Student();
+        Student s2 = new Student("张三");
+        System.out.println(s1.age);
+        System.out.println(s2.age);
+    }
+}
