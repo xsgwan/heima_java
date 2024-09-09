3036d0afa207aa17b8b032189c85d7988c408216
ghh <ghhayl520@gmail.com>
2024-09-09 09:28:16 +0800

    继承

---
+++ project/javasepromax/day01-oop-app/src/com/gyy/Extends/A.java
@@ -0,0 +1,16 @@
+package com.gyy.Extends;
+
+// 父类
+public class A {
+    // 公开成员
+    public int i;
+    public void print1(){
+        System.out.println("==print1==");
+    }
+
+    // 私有成员
+    private int j;
+    private void print2(){
+        System.out.println("==print2==");
+    }
+}
---
+++ project/javasepromax/day01-oop-app/src/com/gyy/Extends/B.java
@@ -0,0 +1,15 @@
+package com.gyy.Extends;
+
+// 子类
+public class B extends A {
+    private int k;
+
+    // 子类是可以继承父类的非私有成员
+    public void print3() {
+        System.out.println(i);
+        print1();
+
+        // System.out.println(j);
+        // print2();
+    }
+}
