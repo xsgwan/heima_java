6111cd8d07572d810f8cc6e6b85a33e3dfb95e0b
ghh <ghhayl520@gmail.com>
2024-09-09 13:57:09 +0800

    继承: 单继承、Object类

---
+++ project/javasepromax/day01-oop-app/src/com/gyy/extends_feature/Test.java
@@ -0,0 +1,23 @@
+package com.gyy.extends_feature;
+
+public class Test {
+    public static void main(String[] args) {
+        // 目标:掌握继承的两个注意事项事项。
+        // 1、Java是单继承的:一个类只能继承一个直接父类;Java中的类不支持多继承，但是支持多层继承。
+        // 2、Object类是Java中所有类的祖宗。
+        A a = new A();
+
+        B b = new B();
+    }
+}
+
+class A {
+} // extends Object{}
+
+class B extends A {
+}
+
+// class C extends B,A{}    不能继承多个父类
+class D extends B {
+} // 支持多层继承
+
