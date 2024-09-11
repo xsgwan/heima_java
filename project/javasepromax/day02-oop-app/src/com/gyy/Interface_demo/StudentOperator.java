eb32acb4eb127cab5c0aa1772212a146133da0ee
ghh <ghhayl520@gmail.com>
2024-09-12 07:03:10 +0800

    接口案例开发: 班级学生信息管理模块

---
+++ project/javasepromax/day02-oop-app/src/com/gyy/Interface_demo/StudentOperator.java
@@ -0,0 +1,9 @@
+package com.gyy.Interface_demo;
+
+import java.util.ArrayList;
+
+public interface StudentOperator {
+    void printAllInfo(ArrayList<Student> students);
+
+    void printAverageScore(ArrayList<Student> students);
+}
