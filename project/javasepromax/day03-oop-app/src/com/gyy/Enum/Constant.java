65e9c8b0ba96ed19cea1045816806d9322b579bd
ghh <ghhayl520@gmail.com>
2024-09-15 05:53:14 +0800

    枚举类: 应用场景

---
+++ project/javasepromax/day03-oop-app/src/com/gyy/Enum/Constant.java
@@ -0,0 +1,7 @@
+package com.gyy.Enum;
+
+public enum Constant {
+//    public static final int BOY = 0;
+//    public static final int GIRL = 1;
+    BOY, GIRL;
+}
---
+++ project/javasepromax/day03-oop-app/src/com/gyy/Enum/Test2.java
@@ -0,0 +1,22 @@
+package com.gyy.Enum;
+
+public class Test2 {
+    public static void main(String[] args) {
+        // 目标: 掌握枚举类的使用场景
+        provideInfo(Constant.GIRL);
+
+    }
+
+    public static void provideInfo(Constant sex) {
+        switch (sex) {
+            case BOY:
+                // 男生
+                System.out.println("展示了一些信息给男生观看");
+                break;
+            case GIRL:
+                // 女生
+                System.out.println("展示了一些信息给女生观看");
+                break;
+        }
+    }
+}
