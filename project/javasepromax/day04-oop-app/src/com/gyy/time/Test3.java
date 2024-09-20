056d07cee4c1136f02d1635869a35171cd77d9b1
ghh <ghhayl520@gmail.com>
2024-09-20 20:04:16 +0800

    JDK8之前传统日期、时间开发案例: 秒杀活动

---
+++ project/javasepromax/day04-oop-app/src/com/gyy/time/Test3.java
@@ -0,0 +1,42 @@
+package com.gyy.time;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Test3 {
+    public static void main(String[] args) throws ParseException {
+        // 目标: 完成秒杀案例
+        // 1、把开始时间、结束时间、小贾下单时间、小皮下单时间拿到程序中来
+        String start = "2023年11月11日 0:0:0";
+        String end = "2023年11月11日 0:10:0";
+        String xj = "2023年11月11日 0:01:18";
+        String xp = "2023年11月11日 0:10:57";
+
+        // 2、把字符串的时间解析成日期对象
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日 H:m:s");
+        Date startDt = sdf.parse(start);
+        Date endDt = sdf.parse(end);
+        Date xjDt = sdf.parse(xj);
+        Date xpDt = sdf.parse(xp);
+
+        // 3、判断小皮、小贾是否秒杀成功
+        // 把日期对象转换成时间毫秒值来判断
+        long startTime = startDt.getTime();
+        long endTime = endDt.getTime();
+        long xjTime = xjDt.getTime();
+        long xpTime = xpDt.getTime();
+
+        if (xjTime >= startTime && xjTime < endTime) {
+            System.out.println("小贾您秒杀成功");
+        } else {
+            System.out.println("小贾您秒杀失败");
+        }
+
+        if (xpTime >= startTime && xpTime < endTime) {
+            System.out.println("小皮您秒杀成功");
+        } else {
+            System.out.println("小皮您秒杀失败");
+        }
+    }
+}
